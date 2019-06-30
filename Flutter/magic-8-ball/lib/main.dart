fb7b7ec8b72a5982b4bcbd639c7c23076b07c81e
Ronan Lamour <ronan.lamour@itga.fr>
2019-06-30 15:54:18 +0200

    finished challenge magic 8 ball

--- Flutter/magic-8-ball/lib/main.dart
+++ Flutter/magic-8-ball/lib/main.dart
@@ -1,7 +1,44 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue.shade900,
+          ),
+          body: Ball(),
+        ),
       ),
     );
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  var ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: FlatButton(
+          onPressed: () {
+            changeBall();
+          },
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),      
+    );
+  }
+
+  void changeBall() {
+  return setState(() {
+    ballNumber = Random().nextInt(5) + 1;
+  });
+}
+}
+
+
